1032c8b4e7adbb878d32defec498c74efa42760c
Joaquim Boura <x-un-i@sapo.pt>
2009-04-28 18:42:32 +0000

    * Non-maintainer upload. * updated greek language file (thks edhunter)

--- sidux-hermes-qt4/translations/sidux-hermes-qt4_el.ts
+++ sidux-hermes-qt4/translations/sidux-hermes-qt4_el.ts
@@ -4,14 +4,14 @@
     <name>QObject</name>
     <message>
         <source>I couldn&apos;t detect any system tray on this system.</source>
-        <translation type="unfinished"></translation>
+        <translation>Δεν μπόρεσα να ανιχνεύσω ένα system tray σε αυτό το σύστημα.</translation>
     </message>
 </context>
 <context>
     <name>SysTray</name>
     <message>
         <source>&amp;Close</source>
-        <translation>&amp;κλείσιμο</translation>
+        <translation>&amp;Κλείσιμο</translation>
     </message>
     <message>
         <source>Forum - Upgrade Warnings</source>
@@ -27,7 +27,7 @@
     </message>
     <message>
         <source>No connection to sidux.com.</source>
-        <translation type="obsolete">Δεν υπάρχει σύνδεση με το sidux.com.</translation>
+        <translation>Δεν υπάρχει σύνδεση με το sidux.com.</translation>
     </message>
     <message>
         <source>No dist-upgrade warnings.</source>
@@ -35,24 +35,25 @@
     </message>
     <message>
         <source>There are dist-upgrade warnings. Please visit sidux.com for more information!</source>
-        <translation type="obsolete">Παρακαλώ επισκεφτείτε το sidux.com για περισσότερες πληροφορίες!</translation>
+        <translation>Παρακαλώ επισκεφτείτε το sidux.com για περισσότερες πληροφορίες!</translation>
     </message>
     <message>
         <source>Status</source>
         <translation>Κατάσταση</translation>
     </message>
     <message>
-        <source>This program warns you, if there are problems in the Debian sid
+        <source>This programm warns you if there a problems in the Debian sid
 repository. It gets the warnings from the sidux homepage.</source>
-        <translation type="unfinished"></translation>
+        <translation>Αυτό το πρόγραμμα σας προειδοποιεί όταν υπάρχουν προβλήματα στα 
+Debian sid αποθετήρια. Τα μηνύματα τα παίρνει από την σελίδα του sidux.</translation>
     </message>
     <message>
         <source>Programmer</source>
-        <translation type="unfinished"></translation>
+        <translation>Προγραμματιστής</translation>
     </message>
     <message>
         <source>Translator</source>
-        <translation type="unfinished"></translation>
+        <translation>Μεταφραστής</translation>
     </message>
     <message>
         <source>NAME OF TRANSLATORS</source>
@@ -68,89 +69,120 @@ repository. It gets the warnings from the sidux homepage.</source>
     </message>
     <message>
         <source>Licence</source>
-        <translation type="unfinished"></translation>
+        <translation>Άδεια</translation>
     </message>
     <message>
         <source>Icons</source>
-        <translation type="unfinished"></translation>
+        <translation>Εικονίδια</translation>
     </message>
     <message>
         <source>Kernel informations</source>
-        <translation type="unfinished"></translation>
+        <translation>Πληροφορίες Πυρήνα</translation>
     </message>
     <message>
         <source>Current kernel</source>
-        <translation type="unfinished"></translation>
+        <translation>Τρέχων Πυρήνας</translation>
     </message>
     <message>
         <source>Newest  kernel</source>
-        <translation type="unfinished"></translation>
+        <translation>Τελευταίος Πυρήνας</translation>
     </message>
     <message>
         <source>Upgradable packages</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>No connection to sidux.com!</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>There are dist-upgrade pre-alerts. Please visit sidux.com for more information!</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>There are dist-upgrade alerts. Please visit sidux.com for more information!</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Version</source>
-        <translation type="unfinished"></translation>
+        <translation>Πακέτα προς αναβάθμιση</translation>
     </message>
 </context>
 <context>
     <name>upgradablePackages</name>
     <message>
         <source>Upgradable Packages</source>
-        <translation type="unfinished"></translation>
+        <translation>Πακέτα προς αναβάθμιση</translation>
     </message>
     <message>
         <source>Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Όνομα</translation>
     </message>
     <message>
         <source>Installed Version</source>
-        <translation type="unfinished"></translation>
+        <translation>Εγκατεστημένη Έκδοση</translation>
     </message>
     <message>
         <source>Latest Version</source>
-        <translation type="unfinished"></translation>
+        <translation>Τελευταία Έκδοση</translation>
+    </message>
+    <message>
+        <source>Reload the current package list of 
+siduxcc-hermes. Use this option if 
+you have updated the package list 
+without siduxcc-hermes.</source>
+        <translation>Ανανέωση της λίστας πακέτων του
+siduxcc-hermes. Χρησιμοποιήστε αυτή την επιλογή 
+αν ενημερώσατε την λίστα πακέτων 
+χωρίς το siduxcc-hermes.</translation>
+    </message>
+    <message>
+        <source>relo&amp;ad</source>
+        <translation>Α&amp;νανέωση</translation>
     </message>
     <message>
         <source>Alt+A</source>
-        <translation type="unfinished"></translation>
+        <translation>Alt+A</translation>
     </message>
     <message>
         <source>package files are only retrieved, not
 unpacked or installed.
 
 Command: apt-get -d dist-upgrade</source>
-        <translation type="unfinished"></translation>
+        <translation>Τα πακέτα απλά κατεβάζονται, δεν
+αποσυμπιέζονται και δεν εγκαθιστώνται.
+
+Εντολή: apt-get -d dist-upgrade</translation>
+    </message>
+    <message>
+        <source>d&amp;ownload</source>
+        <translation>Κ&amp;ατέβασμα</translation>
     </message>
     <message>
         <source>Alt+O</source>
-        <translation type="unfinished"></translation>
+        <translation>Alt+O</translation>
+    </message>
+    <message>
+        <source>update is used to resynchronize the package 
+index files from their sources. The indexes 
+of available packages are fetched from the 
+location(s) specified in /etc/apt/sources.list.
+
+Command: apt-get update</source>
+        <translation>Η ενημέρωση χρησιμοποιείτε για να συγχρονίσει
+τις λίστες πακέτων με τις πηγές τους. Οι λίστες  
+διαθέσιμων πακέτων κατεβάζονται από τις τοποθεσίες 
+που αναφέρονται στο /etc/apt/sources.list. 
+
+Εντολή: apt-get update</translation>
+    </message>
+    <message>
+        <source>&amp;update </source>
+        <translation>&amp;Ενημέρωση</translation>
     </message>
     <message>
         <source>Alt+U</source>
-        <translation type="unfinished"></translation>
+        <translation>Alt+U</translation>
+    </message>
+    <message>
+        <source>upgrade</source>
+        <translation>Αναβάθμιση</translation>
     </message>
     <message>
         <source>&amp;close</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Κλείσιμο</translation>
     </message>
     <message>
         <source>Alt+C</source>
-        <translation type="unfinished"></translation>
+        <translation>Alt+C</translation>
+    </message>
+    <message>
+        <source>Plase wait</source>
+        <translation>Παρακαλώ περιμένετε</translation>
     </message>
     <message>
         <source>To update your system you have to leave the 
@@ -158,33 +190,11 @@ graphic modus. Press for this CTR+ALT+F1,
 login as root and type init 3. After that 
 run the command smxi or siduxcc (Software-&gt;
 Dist-upgrade).</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>d&amp;ownload packages</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>upgrade packages</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>&amp;update list</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Please wait</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>update is used to resynchronize the package 
-index files from their sources. The indexes 
-of available packages are fetched from the 
-location(s) specified in &quot;*.list&quot; configurations files contained into
-the folder /etc/apt/sources.list.d/.
-
-Command: apt-get update</source>
-        <translation type="unfinished"></translation>
+        <translation>Για αναβαθμίσετε το σύστημα σας πρέπει
+να βγείτε από το γραφικό περιβάλλον. Πατήστε CTR+ALT+F1, 
+συνδεθείτε σαν root και δώστε init 3. Μετά από αυτό 
+τρέξτε την εντολή smxi ή siduxcc (Software-&gt;
+Dist-upgrade).</translation>
     </message>
 </context>
-</TS>
+</TS> 
